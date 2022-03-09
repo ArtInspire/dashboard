7bb00d4309b558ee2635c64b30be135362e37e3b
jasminemendoza <60554913+jasminemendoza@users.noreply.github.com>
2022-03-08 17:49:45 -0800

    updating posted prompts

--- src/Seed.js
+++ src/Seed.js
@@ -46,37 +46,37 @@ const DUMMY = {
 			]
 		},
 		{
-			text: 'A house with several dragons flying over it. It is surrounded by a moat of frozen ice, and the outer lands are filled with trees.',
-			likes: 5,
-			tags: ['dragon', 'ice', 'castle'],
+			text: 'A group of superheroes flying over a big city. Each of the heroes has a costume corresponding to their different powers. The city is filled with tall skyscrapers.',
+			likes: 6,
+			tags: ['hero', 'powers', 'city','flying'],
 			username: 'KiranUser',
 			responses: []
 		},
 		{
-			text: 'A boat with several dragons flying over it. It is surrounded by a moat of frozen ice, and the outer lands are filled with trees.',
-			likes: 5,
-			tags: ['dragon', 'ice', 'castle'],
+			text: 'Can someone create something that has a bunch of different animals eating dinner at a table in a restaurant. Their food can be something they typically eat in the wild and they can be wearing suits/clothes.',
+			likes: 2,
+			tags: ['animal', 'food'],
 			username: 'KiranUser',
 			responses: []
 		},
 		{
-			text: 'A city with several dragons flying over it. It is surrounded by a moat of frozen ice, and the outer lands are filled with trees.',
-			likes: 5,
-			tags: ['dragon', 'ice', 'castle'],
+			text: 'One idea I would like to see is drawing cartoon characters in styles from other cartoon shows.',
+			likes: 14,
+			tags: ['cartoon', 'show', 'character'],
 			username: 'KiranUser',
 			responses: []
 		},
 		{
-			text: 'A computer with several dragons flying over it. It is surrounded by a moat of frozen ice, and the outer lands are filled with trees.',
+			text: 'A city in a bubble underwater, like Atlantis in a way.',
 			likes: 5,
-			tags: ['dragon', 'ice', 'castle'],
+			tags: ['water', 'city'],
 			username: 'KiranUser',
 			responses: []
 		},
 		{
-			text: 'A cup with several dragons flying over it. It is surrounded by a moat of frozen ice, and the outer lands are filled with trees.',
-			likes: 5,
-			tags: ['dragon', 'ice', 'castle'],
+			text: 'Art where there are people, but their shadows are different from how they are standing. The shadows can just be different poses or can show something deeper depending on who is creating it.',
+			likes: 8,
+			tags: ['people', 'shadow', 'pose'],
 			username: 'KiranUser',
 			responses: []
 		}
