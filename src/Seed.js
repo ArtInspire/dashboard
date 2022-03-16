652af2685645a78cd184936102ccc5036b3bd59a
Kiran <kirankunigiri@gmail.com>
2022-03-16 15:47:56 -0700

    Added more art posts

--- src/Seed.js
+++ src/Seed.js
@@ -5,7 +5,7 @@ const DUMMY = {
 			text: 'A castle with several dragons flying over it. It is surrounded by a moat of frozen ice, and the outer lands are filled with trees.',
 			likes: 5,
 			tags: ['dragon', 'ice', 'castle'],
-			username: 'KiranUser',
+			username: 'Kiran',
 			responses: [
 				{
 					id: 'response1',
@@ -18,28 +18,28 @@ const DUMMY = {
 				},
 				{
 					id: 'response2',
-					title: 'The Ice Castle 2',
+					title: 'The Dragons Attack!',
 					likes: 2,
-					description: 'I made this castle based off the ice castle from Game of Thrones. I wanted to make it unique by using a pastel art style. I made this castle based off the ice castle from Game of Thrones. I wanted to make it unique by using a pastel art style.',
-					image: 'https://i.imgur.com/BnH9umG.jpeg',
-					username: 'TestUser',
+					description: 'I made this castle based off the ice castle from Game of Thrones. I wanted to make it unique by having the dragons attack the castle using a bright fire breath against the icy mountains.',
+					image: 'https://i.imgur.com/brox2sf.jpg',
+					username: 'JackTheArtist',
 					liked: false
 				},
 				{
 					id: 'response3',
-					title: 'The Ice Castle 3',
+					title: 'Pastel Dragons',
 					likes: 5,
-					description: 'I made this castle based off the ice castle from Game of Thrones. I wanted to make it unique by using a pastel art style.',
-					image: 'https://i.imgur.com/BnH9umG.jpeg',
-					username: 'KiranUser',
+					description: 'I hand painted this castle based off using a more cartoony and abstract art style. I scanned the image to maintain its texture quality!',
+					image: 'https://i.imgur.com/Axd9t4P.jpg',
+					username: 'FantasyArtist',
 					liked: false
 				},
 				{
 					id: 'response4',
-					title: 'The Ice Castle 4',
-					description: 'I made this castle based off the ice castle from Game of Thrones. I wanted to make it unique by using a pastel art style.',
-					image: 'https://i.imgur.com/BnH9umG.jpeg',
-					username: 'KiranUser',
+					title: 'Dark Castle Sketch',
+					description: 'I made a black and white rendition of a castle to give it a more dark style theme. I wanted to have the dragon perched at the top of the castle as if it is the guardian.',
+					image: 'https://i.imgur.com/vbzu3i1.jpg',
+					username: 'Cloverain',
 					likes: 6,
 					liked: false
 				}
@@ -48,41 +48,31 @@ const DUMMY = {
 		{
 			text: 'Draw your OC as a meme',
 			likes: 3,
-			tags: ['OC','meme'],
+			tags: ['OC', 'meme'],
 			username: 'TestUser',
-			responses:[
-				{id:"response9",
-				title:"poppop",
-			deescription: "",
-			username:"TestUser",
-			image:"https://i.imgur.com/6F9z5Mb.png",
-			likes:"3",
-			liked:false}
-			]
+			responses: [{ id: 'response9', title: 'poppop', deescription: '', username: 'TestUser', image: 'https://i.imgur.com/6F9z5Mb.png', likes: '3', liked: false }]
 		},
 		{
 			text: 'A group of superheroes flying over a big city. Each of the heroes has a costume corresponding to their different powers. The city is filled with tall skyscrapers.',
 			likes: 6,
-			tags: ['hero', 'powers', 'city','flying'],
-			username: 'KiranUser',
-			responses: [
-				
-			]
+			tags: ['hero', 'powers', 'city', 'flying'],
+			username: 'Kiran',
+			responses: []
 		},
 		{
 			text: 'Can someone create something that has a bunch of different animals eating dinner at a table in a restaurant. Their food can be something they typically eat in the wild and they can be wearing suits/clothes.',
 			likes: 2,
 			tags: ['animal', 'food'],
-			username: 'KiranUser',
+			username: 'Kiran',
 			responses: [
 				{
-					id:"response6",
-					title:"animal restaurant",
-					description:"panda, cat, bunny",
-					username:"Cloverain",
-					image:"https://i.imgur.com/l9GRhNM.png",
-					likes:1,
-					liked:false
+					id: 'response6',
+					title: 'animal restaurant',
+					description: 'panda, cat, bunny',
+					username: 'Cloverain',
+					image: 'https://i.imgur.com/l9GRhNM.png',
+					likes: 1,
+					liked: false
 				}
 			]
 		},
@@ -90,28 +80,21 @@ const DUMMY = {
 			text: 'One idea I would like to see is drawing cartoon characters in styles from other cartoon shows.',
 			likes: 14,
 			tags: ['cartoon', 'show', 'character'],
-			username: 'KiranUser',
+			username: 'Kiran',
 			responses: []
 		},
 		{
 			text: 'A city in a bubble underwater, like Atlantis in a way.',
 			likes: 5,
 			tags: ['water', 'city'],
-			username: 'KiranUser',
-			responses: [
-			{id:'response5',
-			title:"underwater city",
-			description:" ",
-			username: "Cloverain",
-			image:'https://i.imgur.com/ZWOMGEM.png',
-			likes: 2,
-			liked: false},]
+			username: 'Kiran',
+			responses: [{ id: 'response5', title: 'underwater city', description: ' ', username: 'Cloverain', image: 'https://i.imgur.com/ZWOMGEM.png', likes: 2, liked: false }]
 		},
 		{
 			text: 'Art where there are people, but their shadows are different from how they are standing. The shadows can just be different poses or can show something deeper depending on who is creating it.',
 			likes: 8,
 			tags: ['people', 'shadow', 'pose'],
-			username: 'KiranUser',
+			username: 'Kiran',
 			responses: []
 		}
 	],
@@ -121,12 +104,22 @@ const DUMMY = {
 			image: 'https://i.imgur.com/A45Gg4c.jpg'
 		},
 		{
-			username: 'KiranUser',
+			username: 'Kiran',
 			image: 'https://i.imgur.com/BwxFL7M.jpeg'
 		},
-		{username: "Cloverain",
-		image:'https://i.imgur.com/L0coBkP.jpg'}
+		{ 
+			username: 'Cloverain', 
+			image: 'https://i.imgur.com/L0coBkP.jpg' 
+		},
+		{
+			username: 'JackTheArtist',
+			image: 'https://i.imgur.com/hS0BMnx.png'
+		},
+		{
+			username: 'FantasyArtist',
+			image: 'https://i.imgur.com/ewXh6pF.jpg'
+		},
 	]
 }
 
-export default DUMMY;
+export default DUMMY
