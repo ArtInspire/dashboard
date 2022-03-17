c8269152d75e17c5869b2c7d954f3248f9d3056b
Kiran <kirankunigiri@gmail.com>
2022-03-16 18:14:16 -0700

    Opening an image in full screen now fits to the screen
    Previously, opening in image in full screen could force the user to scroll to view larger images. Now the size of the fullscreen modal is limited to the size of the device screen.

--- src/Components/UI/Image.js
+++ src/Components/UI/Image.js
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
 import { Modal } from '@mantine/core';
+import '../Post/PostDetail.css'
 
 function Image(props) {
 
@@ -11,8 +12,9 @@ function Image(props) {
 
 	return (
 		<Fragment>
-			<Modal size="min(120vh, 85vw)" opened={opened} onClose={() => setOpened(false)} title="Image Response">
-				<img style={{height: "100%", width: "100%", objectFit: "contain"}} src={props.src} alt={props.alt} />
+			{/* size="max(70vh, 85vw)" */}
+			<Modal className="image-modal" opened={opened} onClose={() => setOpened(false)} title="Image Response">
+				<img style={{width: "100%", objectFit: "contain"}} src={props.src} alt={props.alt} />
 			</Modal>
 			<img style={{cursor: "pointer"}} onClick={imageClickHandler} className={props.className} src={props.src} alt={props.alt} />
 		</Fragment>
